ca9c52d1b5177c37fbc9edd9a1a20ae6d0713d78
lawrenzo-pro <57544775+lawrenzo-pro@users.noreply.github.com>
2020-06-22 12:58:36 +0300

    src

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+mod vga_buffer;
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    panic!("Error:Not an Error!");
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
