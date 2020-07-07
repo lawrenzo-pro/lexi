de168d803995377f5b11af39cc1751b9391154eb
Larry Kimani <lawrencemacharia40@gmail.com>
2020-07-07 16:02:54 +0300

    added lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
 use core::panic::PanicInfo;
-
+use lexi::println;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Error:Not an Error!");
+
+    #[cfg(test)]
+    test_main();
     loop {}
 }
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    lexi::test_panic_handler(info)
 }
